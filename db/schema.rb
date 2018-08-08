22264740eeb9d8f213e5873b300c14f644e5dab7
rakesh kumar <rakeshrajput537@gmail.com>
2018-08-08 17:55:37 +0530

    root students/index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_072154) do
+ActiveRecord::Schema.define(version: 2018_08_08_121121) do
 
   create_table "students", force: :cascade do |t|
     t.string "Name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_072154) do
     t.integer "mobile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
